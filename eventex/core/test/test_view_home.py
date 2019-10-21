e0609585da72fb3d047593cae17f5282e5a011ca
Renan Diniz Smaal <renan.diniz@instruct.com.br>
2019-10-21 19:44:22 -0300

    Refatora testes e templates novamente

--- eventex/core/test/test_view_home.py
+++ eventex/core/test/test_view_home.py
@@ -6,9 +6,11 @@ class HomeTest(TestCase):
         self.response = self.client.get('/')
 
     def test_get(self):
+        """GET / must return status code 200"""
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
+        """Must use index.html"""
         self.assertTemplateUsed(self.response, 'index.html')
 
     def test_subscription_link(self):
---
+++ eventex/subscriptions/test/test_mail_subscribe.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from django.core import mail
+
+
+class SubscribePostValid(TestCase):
+    def setUp(self):
+        data = dict(name='Renan Smaal', cpf='12345678910',
+                    email='renan@smaal.net', phone='19-99292-9292')
+        self.resp = self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+
+    def test_subscriptions_email_subject(self):
+        expect = 'Confirmação de inscrição'
+
+        self.assertEqual(expect, self.email.subject)
+
+    def test_subscription_email_form(self):
+        expect = 'contato@eventex.com.br'
+
+        self.assertEqual(expect, self.email.from_email)
+
+    def test_subscription_email_to(self):
+        expect = ['contato@eventex.com.br', 'renan@smaal.net']
+
+        self.assertEqual(expect, self.email.to)
+
+    def test_subscription_email_body(self):
+        contents = [
+            'Renan Smaal',
+            '12345678910',
+            'renan@smaal.net',
+            '19-99292-9292',
+        ]
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.email.body)
