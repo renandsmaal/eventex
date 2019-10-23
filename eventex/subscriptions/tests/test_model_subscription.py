44024e06df087144d232cbf75a11c70e87f21b32
Renan Diniz Smaal <renan.diniz@instruct.com.br>
2019-10-22 22:16:50 -0300

    Admin magic

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -7,7 +7,7 @@ from eventex.subscriptions.models import Subscription
 class SubscriptionModelTest(TestCase):
     def setUp(self):
         self.obj = Subscription(
-            name='Renan SMaal',
+            name='Renan Smaal',
             cpf='12345678910',
             email='renan@smaal.net',
             phone='19-99292-9292'
@@ -21,3 +21,6 @@ class SubscriptionModelTest(TestCase):
     def test_created_at(self):
         """Subscription must have an auto created_at attribute"""
         self.assertIsInstance(self.obj.created_at, datetime)
+
+    def test_str(self):
+        self.assertEqual('Renan Smaal', str(self.obj))
